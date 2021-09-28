c5d8eebf3138ff43a157f1f2f8ee3952551634fa
gilbertomoj <gibamedeirosgc@gmail.com>
2021-09-27 22:05:05 -0300

    Atividade

---
+++ 1/index.js
@@ -0,0 +1,14 @@
+//1
+// var tabuada = []
+// for(var i = 1; i != 11; i ++){
+//     tabuada.push(i*5)
+// }
+// var tabuadaString = tabuada.join()
+// console.log(tabuadaString)
+
+//2
+// var temp = [54, 78, 56, 98, 0, 12, 11, 37]
+// for(var i = 0; i < temp.length ; i ++){
+//     var celsius = (temp[i] - 32 ) / 1.8
+//     console.log(`Fahrenheit : ${temp[i]}\nCelsius : ${celsius}`)
+// }
